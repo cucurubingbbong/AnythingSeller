f3766210691ab863b72cc515fdd8e3c9afab5d50
HAEBI <haebi0215@gmail.com>
2026-03-25 17:23:11 +0900

    랜덤값 2/3

--- Anyting_seller/Assets/Scripts/Manager/NPCManager.cs
+++ Anyting_seller/Assets/Scripts/Manager/NPCManager.cs
@@ -1,4 +1,3 @@
-using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -8,16 +7,44 @@ public class NPCManager : MonoBehaviour
     public float mood = 100;
     public float currentMood = 0.0f;
 
-    public NPCState emotionState;
+    NPCState.EmotionState eState;
+    NPCState.CustomerType cType;
+
+    public GameObject requireItem;
 
     void Start()
     {
-        currentMood = Random.Range(50, 100);
-        Array values = Enum.GetValues(typeof())
+        SetEState();
+        SetCState();
     }
 
     void Update()
     {
         
+    }
+
+    void SetEState()
+    {
+        currentMood = Random.Range(40, 100);
+
+        if (currentMood >= 90)
+            eState = NPCState.EmotionState.Happy;
+        else if (currentMood >= 70)
+            eState = NPCState.EmotionState.NotBad;
+        else if (currentMood >= 60)
+            eState = NPCState.EmotionState.SoSo;
+        else if (currentMood >= 50)
+            eState = NPCState.EmotionState.Normal;
+        else
+            eState = NPCState.EmotionState.ListLess;
+        
+
+        Debug.Log(eState);
+    }
+    void SetCState()
+    {
+        var cValues = System.Enum.GetValues(typeof(NPCState.CustomerType));
+        int randomCType = Random.Range(0, cValues.Length);
+        Debug.Log(randomCType);
     }
 }
--- Anyting_seller/Assets/Scripts/Type/NPCState.cs
+++ Anyting_seller/Assets/Scripts/Type/NPCState.cs
@@ -6,9 +6,26 @@ public class NPCState : MonoBehaviour
 {
     public enum EmotionState
     {
-        Normal = 0,
-        Happy = 1,
-        Angry = 2,
-        Sad = 3
+        None = 0,
+        Normal = 1,
+        Happy = 2,
+        NotBad = 3,
+        Angry = 4,
+        Sad = 5,
+        ListLess = 6, //Listless: 기운이 하나도 없고 만사가 귀찮은 상태를 나타내는 형용사입니다.
+        SoSo = 7,
     }
+    EmotionState EState = EmotionState.None;
+
+    public enum CustomerType
+    {
+        None = 0,
+        Haggler = 1, // 비싸다는놈
+        Rude = 2, // 무례한놈
+        Dirty = 3, // 노숙자
+        Foreigner = 4, // 외국인
+        Drunk = 5, // 술취한놈
+        Thrower = 6 // 물건던지는놈
+    }
+    CustomerType CState = CustomerType.None;
 }
